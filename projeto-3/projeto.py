4fced5ba5022c05254ad9d10de32b5bf01b52aa9
Felippe Andrade <4drade@gmail.com>
2022-10-10 21:49:01 -0300

    correções e melhorias

--- projeto-3/projeto.py
+++ projeto-3/projeto.py
@@ -1,8 +1,8 @@
-# FORCA
+# JOGO DA FORCA
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from unidecode import unidecode
+from unidecode import unidecode  # remover acento da palavra
 
 # opções do navegador
 options = webdriver.ChromeOptions()
@@ -10,9 +10,10 @@ options.add_argument('--headless')
 # abrir navegador
 navegador = webdriver.Chrome(options=options)
 # gerar palavra
-navegador.get('https://www.palabrasaleatorias.com/palavras-aleatorias.php?fs=1&fs2=0&Submit=Nova+palavra')
-palavra = navegador.find_element(By.XPATH, '/html/body/center/center/table[1]/tbody/tr/td/div').text
+navegador.get('https://www.palavrasque.com/palavra-aleatoria.php?Submit=Nova+palavra')
+palavra = navegador.find_element(By.XPATH, '/html/body/center[2]/font/font/b').text
 # formatar a palavra
+palavra_backup = palavra.strip().lower()
 palavra = unidecode(palavra).strip().lower()
 # fechar navegador
 navegador.quit()
@@ -20,38 +21,39 @@ navegador.quit()
 # jogo
 digitadas = []
 erros = []
-vidas = 6
+chances = 6
 
-while vidas > 0:
-    letra = input('Digite uma letra: ').lower()[0]
+while chances > 0:
+    letra = input('Digite uma letra: ').lower()
 
-    if letra in palavra:
-        if digitadas.count(letra) == 0:
-            print(f'wow, a letra "{letra}" existe na palavra secreta.')
-            digitadas.append(letra)
-        else:
-            print(f'ops, a letra "{letra}" já foi dita, escolha uma diferente.\n')
-            continue
+    if letra:
+        letra = letra[0]
     else:
-        if letra not in erros:
-            print(f'aff, a letra "{letra}" não existe na palavra secreta.')
-            erros.append(letra)
-            vidas -= 1
-            print(f'\033[31mvidas: {vidas}\033[m\n')
-        else:
-            print(f'ops, a letra "{letra}" já foi dita, escolha uma diferente.\n')
         continue
 
+    if letra in palavra and digitadas.count(letra) == 0:
+        print(f'\033[32mwow, a letra "{letra}" exista na palavra secreta.\033[m')
+        digitadas.append(letra)
+    elif letra not in erros:
+        print(f'\033[31maff, a letra "{letra}" não existe na palavra secreta.\033[m')
+        chances -= 1
+        erros.append(letra)
+    else:
+        print(f'\033[33mops, a letra "{letra}" já foi dita, escolha uma diferente.\033[m')
+    print(f'você ainda tem {chances} chances.')
+
     palavra_temp = ''
+
     for letra_temp in palavra:
         if letra_temp in digitadas:
             palavra_temp += letra_temp
         else:
             palavra_temp += '_'
 
-    print(f'\033[32m{palavra_temp}\033[m', end='\n\n')
+    print(f'a palavra secreta está assim: \033[1;7m{palavra_temp}\033[m\n')
+
     if palavra_temp == palavra:
-        print('\033[32mVocê conseguiu acertar a palavra. Parabéns!\033[m')
+        print(f'\033[1;32mVocê conseguiu acertar a palavra! A palavra secreta era "{palavra_backup}"\033[m')
         break
 else:
-    print(f'\033[31mSuas chances acabaram. A palavra escolhida era "{palavra}"\033[m')
+    print(f'\033[1;31mSuas chances acabaram. A palavra secreta era "{palavra_backup}"\033[m')
